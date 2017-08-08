4c74b89a6adc8c6d9579aa4265028ef34147601b
kennli <kennli@visa.com>
2017-08-08 14:14:51 -0700

    Additional tests for AccountRepository save functionality

--- src/test/java/com/visa/ncg/canteen/AccountRepositoryTests.java
+++ src/test/java/com/visa/ncg/canteen/AccountRepositoryTests.java
@@ -8,7 +8,6 @@ public class AccountRepositoryTests {
 
     //findOne
     //findAll
-    //save
 
     public AccountRepositoryTests() {
     }
@@ -19,5 +18,30 @@ public class AccountRepositoryTests {
         Account account = new Account("New Account",0,null);
         repo.save(account);
         assertThat(account.getId()).isNotNull();
+    }
+
+    @Test
+    public void saveExistingAccountReturnsSameId() throws Exception {
+        AccountRepository repo = new AccountRepository();
+        Account account = new Account("New Account",0,null);
+        repo.save(account);
+
+        Long expectedAccountId = account.getId();
+        repo.save(account);
+        Long existingAccountId = account.getId();
+
+        assertThat(existingAccountId).isEqualTo(expectedAccountId);
+    }
+
+    @Test
+    public void saveTwoUniqueAccountsReturnsUniqueIds() throws Exception {
+        AccountRepository repo = new AccountRepository();
+        Account account1 = new Account("Account 1", 0, null);
+        Account account2 = new Account("Account 2", 0, null);
+
+        repo.save(account1);
+        repo.save(account2);
+
+        assertThat(account1.getId()).isNotEqualTo(account2.getId());
     }
 }
