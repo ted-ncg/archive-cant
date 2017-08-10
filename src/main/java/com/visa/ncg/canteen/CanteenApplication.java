0ece15ee1b802c8696bcee6bf8891ef8fcdcf390
kennli <kennli@visa.com>
2017-08-10 11:44:15 -0700

    Add basic account view + utilized AccountRepository

--- src/main/java/com/visa/ncg/canteen/CanteenApplication.java
+++ src/main/java/com/visa/ncg/canteen/CanteenApplication.java
@@ -5,19 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Random;
+
 @SpringBootApplication
 public class CanteenApplication {
 
-  public static void main(String[] args) {
-      SpringApplication.run(CanteenApplication.class, args);
-  }
-
-  @Bean
-  public AccountRepository createAccountRepository() {
-    AccountRepository accountRepository = new AccountRepository();
-    for(int i = 1; i <= 200; i++) {
-        accountRepository.save(new Account("Account" + i,i));
+    public static void main(String[] args) {
+        SpringApplication.run(CanteenApplication.class, args);
     }
-    return accountRepository;
-  }
+
+    @Bean
+    public AccountRepository createAccountRepository() {
+        AccountRepository accountRepository = new AccountRepository();
+        Random rand = new Random();
+        for (int i = 1; i <= 200; i++) {
+            accountRepository.save(new Account("Account" + i, Math.abs(rand.nextInt()+1)));
+        }
+        return accountRepository;
+    }
 }
--- src/main/java/com/visa/ncg/canteen/RootController.java
+++ src/main/java/com/visa/ncg/canteen/RootController.java
@@ -1,13 +1,27 @@
 package com.visa.ncg.canteen;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RootController {
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public RootController(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 
     @GetMapping("/")
     public String root() {
         return "index";
+    }
+
+    @GetMapping("/account")
+    public String accountView(Model model) {
+        model.addAttribute("account", accountRepository.findOne(1L));
+        return "account-view";
     }
 }
