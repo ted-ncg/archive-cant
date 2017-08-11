32f7ce89848ed83d36d7d206bd4582cc377eb4d0
VISA\jincao <jincao@visa.com>
2017-08-11 10:46:58 -0700

    Add Withdraw Page.

--- src/main/java/com/visa/ncg/canteen/WebController.java
+++ src/main/java/com/visa/ncg/canteen/WebController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController {
@@ -25,5 +27,32 @@ public class WebController {
     public  String allAccounts(Model model){
         model.addAttribute("accounts", accountRepository.findAll());
         return "accounts";
+    }
+
+    @GetMapping("/withdraw/{id}")
+    public String withdrawGet(Model model,
+                              @PathVariable("id") long id) {
+        // put the Account into the model
+        model.addAttribute("account",accountRepository.findOne(id));
+
+        // create a new WithdrawForm and set its accountId
+        WithdrawForm withdrawFormObject = new WithdrawForm();
+        withdrawFormObject.setAccountId(id);
+
+        model.addAttribute("withdrawForm", withdrawFormObject);
+        return "withdraw";
+    }
+
+    @PostMapping("/withdraw")
+    public String withdrawPost(@ModelAttribute WithdrawForm form) {
+        // get the account ID from the form
+        long accountID = form.getAccountId();
+
+        AccountService service = new AccountService();
+        // execute the withdraw on that account via the service
+
+        service.withdraw(accountRepository.findOne(accountID), form.getAmount());
+        
+        return "redirect:/account/" + form.getAccountId();
     }
 }
---
+++ src/main/java/com/visa/ncg/canteen/WithdrawForm.java
@@ -0,0 +1,24 @@
+package com.visa.ncg.canteen;
+
+
+public class WithdrawForm {
+    private  long accountId;
+    private  int amount;
+
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
