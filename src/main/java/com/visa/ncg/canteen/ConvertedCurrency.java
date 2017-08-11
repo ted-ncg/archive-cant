e54b1640826bba2487a0e6ee401532e443f50dde
Jose, Dona Maria <djose@visa.com>
2017-08-11 13:28:56 -0700

    Currency conversion

---
+++ src/main/java/com/visa/ncg/canteen/ConvertedCurrency.java
@@ -0,0 +1,26 @@
+package com.visa.ncg.canteen;
+
+/**
+ * Created by djose on 8/11/17.
+ */
+public class ConvertedCurrency {
+
+    String currency;
+    int converted;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public int getConverted() {
+        return converted;
+    }
+
+    public void setConverted(int converted) {
+        this.converted = converted;
+    }
+}
